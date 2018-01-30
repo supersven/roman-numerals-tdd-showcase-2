90798472357e5aa627d93a32bacffc9a69253e21
Sven Tennie <sven.tennie@gmail.com>
2018-01-30 14:50:40 +0100

    Refactor: Use loop to remove duplication.

--- src/main/java/org/supersven/kata/numerals/roman/RomanNumerals.java
+++ src/main/java/org/supersven/kata/numerals/roman/RomanNumerals.java
@@ -5,17 +5,7 @@ public class RomanNumerals {
     public static String convert(int arabic) {
         String result = "";
 
-        if (arabic == 3) {
-            result = result + "I";
-            arabic = arabic - 1;
-        }
-
-        if (arabic == 2) {
-            result = result + "I";
-            arabic = arabic - 1;
-        }
-
-        if (arabic == 1) {
+        while (arabic > 0) {
             result = result + "I";
             arabic = arabic - 1;
         }
